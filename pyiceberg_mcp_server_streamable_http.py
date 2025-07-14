543360436792aaa4c6e3c4352fb344b8ed76922e
Diptiman Raichaudhuri <draichaudhuri@confluent.io>
2025-07-14 14:43:16 +0530

    Update pyiceberg_mcp_server_streamable_http.py

--- pyiceberg_mcp_server_streamable_http.py
+++ pyiceberg_mcp_server_streamable_http.py
@@ -1,75 +1,45 @@
-from mcp.server.fastmcp import FastMCP
-from pyiceberg.catalog import load_catalog
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_anthropic import ChatAnthropic
+import asyncio
 
+from fastmcp.client.transports import StreamableHttpTransport
+from fastmcp import Client
 
-#warehouse_path = "/Users/diptimanraichaudhuri/testing_space/iceberg_playground/dw1"
-catalog_path = "/Users/diptimanraichaudhuri/testing_space/iceberg_playground/"
-catalog = load_catalog("pyarrowtest",
-                    **{
-                        "uri": f"sqlite:///{catalog_path}/pyiceberg_catalog/pyarrow_catalog.db"
-                    })
+from dotenv import load_dotenv
 
-mcp = FastMCP("PyIceberg MCP",  host="localhost", port=31267)
+load_dotenv()
 
+model = ChatAnthropic(
+    model="claude-sonnet-4-20250514",
+    temperature=0,
+    max_tokens=1024,
+    timeout=None,
+    max_retries=2
+)
 
-@mcp.tool()
-async def describe_catalog_properties():
-    """
-        Retrieve Iceberg catalog properties from the sqlite catalog.
-        Prints catalog properties
-    """
-    return {
-        "catalog_properties": catalog.properties
-    }
-    
-@mcp.tool()
-async def list_namespaces():
-    """
-        Retrieve namespaces from Iceberg catalog.
-        Print namesapces
-    """
-    return {
-        "namespaces": catalog.list_namespaces()
-    }
+async def main():
+        try:
+            transport = StreamableHttpTransport(url="http://localhost:31267/mcp")
+            async with Client(transport=transport) as client:
+                # List available tools
+                tools = await load_mcp_tools(client.session)
+                print(f"Loaded Tools: {[tool.name for tool in tools]}")
 
-    
-@mcp.tool()
-async def list_tables(namesapce):
-    """
-        List tables from the namesapce
-        Prints tables
-    """
-    return {
-        "tables": catalog.list_tables(namesapce)
-    }
-    
+                agent = create_react_agent(model, tools)
+                print("ReAct Agent Created.")
 
-@mcp.tool()
-async def detect_partitions(table):
-    """
-        Check the partition of the table
-        Print the name of the partition and how may partitions are there for the table
-    """
-    part_table_ = catalog.load_table(table)
-    return {
-        "partitions": part_table_.inspect.partitions()
-    }
-    
-@mcp.tool()
-async def query(table):
-    """
-        Query the Iceberg table
-        Print the result in tabular form
-    """
-    q_table = catalog.load_table(table)
-    return {
-        "result": q_table.scan().to_pandas()
-    }
-    
+                print(f"Invoking agent with query")
+                response = await agent.ainvoke({
+                    "messages": [("user",
+                                  "list namespaces and tables from Iceberg catalog.\n"
+                                  "Then query the table and return the device_make of the device that consumed the highest charge.")]
+                })
+                return response["messages"][-1].content
+        finally:
+            client.close()
 
-if __name__ =="__main__":
-    print("Starting PyIceberg MCP Server with Stremable HTTP Transport Mode....")
-    mcp.run(transport="streamable-http")
-
-
-
+if __name__ == "__main__":
+    result = asyncio.run(main())
+    print("\nAgent Final Response:")
+    print(result)
